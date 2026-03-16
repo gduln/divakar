9c20e94ea62e18d98a9659737058af1d4c87972a
gduln <divakar.gorle@gmail.com>
2026-03-16 09:01:13 +0530

    modified code

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Divakar! DevOps is working 🚀');
+  res.send('Hello Divakar! DevOps is working ');
+});
+app.get('/hi',(req,res) => {
+  res.send('this modified data in devops by divakar naidu');
 });
 
 app.listen(PORT, () => {
